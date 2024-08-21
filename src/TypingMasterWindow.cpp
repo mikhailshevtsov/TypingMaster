949d5d594897d7de240a4d83593f2c1462b55d4e
mikhailshevtsov <mikhail.shevtsoff@gmail.com>
2024-08-21 11:12:07 +0300

    Added basic keyboard input events handler

--- src/TypingMasterWindow.cpp
+++ src/TypingMasterWindow.cpp
@@ -1,7 +1,53 @@
 #include "TypingMasterWindow.hpp"
 
-TypingMasterWindow::TypingMasterWindow(QWidget *parent)
+#include "TypingLine.hpp"
+
+#include <QKeyEvent>
+
+TypingMasterWindow::TypingMasterWindow(QWidget* parent)
     : QMainWindow(parent)
-{}
+{
+    mTypingLine = new TypingLine;
+    mTypingLine->resize(1000, 300);
+    mTypingLine->setTypingText("Enter to Start ", " Escape to Stop");
+    setCentralWidget(mTypingLine);
+
+    mTypingSource.open("../../phrases.en");
+}
 
 TypingMasterWindow::~TypingMasterWindow() {}
+
+void TypingMasterWindow::keyPressEvent(QKeyEvent* event)
+{
+    switch (mState)
+    {
+    case State::Inactive:
+        if (event->key() == Qt::Key_Return || event->key() == Qt::Key_Space) // begin typing session
+        {
+            mState = State::Active;
+            mTypingLine->setTypingText(mTypingSource.prev(), mTypingSource.next());
+        }
+        break;
+
+    case State::Active:
+        if (event->key() == Qt::Key_Escape || mTypingSource.atEnd()) // pause or finish typing session
+        {
+
+        }
+        else if (event->text().size() == 1) // user typed char
+        {
+            QChar typedChar = event->text().front();
+            if (typedChar == mTypingSource.peekChar())
+            {
+                mTypingSource.nextChar();
+                mTypingLine->setTypingText(mTypingSource.prev(), mTypingSource.next());
+            }
+            else
+            {
+
+            }
+        }
+        break;
+    }
+    QMainWindow::keyPressEvent(event);
+}
--- src/TypingMasterWindow.hpp
+++ src/TypingMasterWindow.hpp
@@ -1,6 +1,9 @@
 #ifndef TYPINGMASTERWINDOW_H
 #define TYPINGMASTERWINDOW_H
 
+#include "TypingSource.hpp"
+class TypingLine;
+
 #include <QMainWindow>
 
 class TypingMasterWindow : public QMainWindow
@@ -10,5 +13,14 @@ class TypingMasterWindow : public QMainWindow
 public:
     TypingMasterWindow(QWidget* parent = nullptr);
     ~TypingMasterWindow();
+
+    enum class State { Active, Inactive };
+    void keyPressEvent(QKeyEvent* event) override;
+
+private:
+    State mState = State::Inactive;
+
+    TypingLine* mTypingLine;
+    TypingSource mTypingSource;
 };
 #endif // TYPINGMASTERWINDOW_H
